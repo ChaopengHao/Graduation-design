0240e51102b6a0a7fe6d2a50fe396b8304ca748d
ChaopengHao <2013220402026@std.uestc.edu.cn>
2017-04-05 20:58:18 +0800

    修改爬虫

--- JingdongSpider/JingdongSpider/spiders/jd_spider.py
+++ JingdongSpider/JingdongSpider/spiders/jd_spider.py
@@ -1,11 +1,23 @@
 import scrapy
+from scrapy.selector import Selector
 
 class JDSpider(scrapy.Spider):
     name = "jdspider"
     start_urls = []
-    for i in range(1,120):
+    for i in range(1,2):
         url = "http://list.jd.com/list.html?cat=9987,653,655&page=" + str(i)
+        print url
         start_urls.append(url)
 
     def parse(self, response):
-        pass
+        selector = Selector(response=response)
+        goods = selector.xpath('//li[@class="gl-item"]')
+        for good in goods:
+            print good.xpath('./div/div[@class="p-name"]/a/em/text()').extract()
+            print good.xpath('./div/div[@class="p-shop"]/@data-shop_name').extract()
+            print good.xpath('./div/div[@class="p-img"]/a/@href').extract()
+            print good.xpath('./div/@data-sku').extract()
+            # print good.xpath('./div/div[@class="p-name"]/a/i').extract()
+            # print good.xpath('./div/div[@class="p-name"]').extract()
+            # print good.xpath('./div/div[@class="p-price"]').extract()
+            print "good"
